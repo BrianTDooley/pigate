a0f7c522d33d87686224ce4081e01f347e3c0307
BrianTDooley <brian.dooley@gmail.com>
2019-12-23 14:45:01 +0000

    adds ping & lightbulb functionality

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
  var express = require("express");
  var app = express();
  var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
- var ZAPPER = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+ var zapper = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
+ var Gpio2 = require('onoff').Gpio; //include onoff to interact with the GPIO
+ var ping = new Gpio2(26, 'out'); //use GPIO pin 26, and specify that it is outp$
 
  /* serves zapper page */
  app.get("/zapper", function(req, res) {
-    console.log('i have been zapped');
-    ZAPPER.writeSync(1); // Turn ZAPPER on 
+    console.log(new Date(), 'i have been zapped');
+    zapper.writeSync(1); // Turn ZAPPER on 
     sleep(4000);
-    ZAPPER.writeSync(0); // Turn ZAPPER off
+    zapper.writeSync(0); // Turn ZAPPER off
     res.sendFile('./index.htm' , { root : __dirname});
  });
+
+ /* serves zapper page */
+ app.get("/ping", function(req, res) {
+    console.log(new Date(), 'i have been pinged');
+    ping.writeSync(1); // Turn ping on 
+    sleep(1000);
+    ping.writeSync(0); // Turn ping off
+    res.json({msg: 'ok'});
+ });
+
 
  /* serves all the static files */
  app.get(/^(.+)$/, function(req, res){ 
